78b537a27eda23c0dc082cdac540e341c18cfb1f
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-10-13 23:49:32 -0400

    woops use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
-# from setuptools import setup
-
-# from distutils.
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension
 
 import numpy as np
 from Cython.Build import cythonize
